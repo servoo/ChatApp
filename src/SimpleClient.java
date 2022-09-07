d9a177029f25caaed49a9759c397dcc292d138d9
servoo <servettakmaz@gmail.com>
2022-09-07 20:32:10 +0200

    Add reading thread logic

--- src/SimpleClient.java
+++ src/SimpleClient.java
@@ -2,19 +2,27 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
 public class SimpleClient {
 
     JTextField outGoing;
+    JTextArea incoming;
+    BufferedReader reader;
     PrintWriter writer;
     Socket socket;
 
     public void go() {
         JFrame frame = setupGUI();
         setupNetworking();
+
+        Thread readerThread = new Thread(new IncomingReader());
+        readerThread.start();
+
         frame.setSize(400, 500);
         frame.setVisible(true);
     }
@@ -22,10 +30,15 @@ public class SimpleClient {
     private JFrame setupGUI() {
         JFrame frame = new JFrame("Simple Chat Client");
         JPanel mainPanel = new JPanel();
+
         outGoing = new JTextField(20);
+        incoming = new JTextArea(20, 20);
+        incoming.setEditable(false);
         JButton sendButton = new JButton("Send");
+
         sendButton.addActionListener(new SendButtonListener());
 
+        mainPanel.add(incoming);
         mainPanel.add(outGoing);
         mainPanel.add(sendButton);
 
@@ -36,6 +49,8 @@ public class SimpleClient {
     private void setupNetworking() {
         try {
             socket = new Socket("127.0.0.1", 4242);
+            InputStreamReader streamReader = new InputStreamReader(socket.getInputStream());
+            reader = new BufferedReader(streamReader);
             writer = new PrintWriter(socket.getOutputStream());
             System.out.println("Connection established");
         } catch (IOException e) {
@@ -63,5 +78,20 @@ public class SimpleClient {
     public static void main(String[] args) {
         SimpleClient client = new SimpleClient();
         client.go();
+    }
+
+    private class IncomingReader implements Runnable {
+        @Override
+        public void run() {
+            String message;
+            try {
+                System.out.println("Reading!");
+                while ((message = reader.readLine()) != null) {
+                    incoming.append(message + "\n");
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
